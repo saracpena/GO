c591aa85cc7cc3a795d1e5760cd6d6a2effb4c57
Sara Peña <s.cristal.p@gmail.com>
2021-03-21 19:49:55 -0400

    Update main.go

--- HELLO-WORLD/main.go
+++ HELLO-WORLD/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import "log"
 
-func main(){
-	fmt.Println("Hello, World of GO!")
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
 }
+
+type Dog struct {
+	Name string
+	Breed string
+}
+
+type Gorilla struct {
+	Name string
+	Color string
+	NumberOfTeeth int
+}
+
+func main() {
+	dog := Dog {
+		Name: "Samson",
+		Breed: "German Shepherd",
+	}
+
+	PrintInfo(dog)
+
+	gorilla := Gorilla {
+		Name: "King Kong",
+		Color: "Black",
+		NumberOfTeeth: 32,
+	}
+
+	PrintInfo(gorilla)
+}
+
+func (d Dog) Says() string {
+	return "woof"
+}
+
+func (d Dog) NumberOfLegs() int{
+	return 4
+}
+func (d Gorilla) Says() string {
+	return "grunt"
+}
+
+func (d Gorilla) NumberOfLegs() int{
+	return 2
+}
+
+func PrintInfo(a Animal){
+	log.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+
+// import "fmt"
+
+// func main(){
+// 	fmt.Println("Hello, World of GO!")
+// }
