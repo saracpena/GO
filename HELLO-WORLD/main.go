8a4bf7b23424c7a519ab92b84a3a880d24b4788f
Sara Peña <s.cristal.p@gmail.com>
2021-03-21 20:12:19 -0400

    Created "Hello World" Web App

--- HELLO-WORLD/main.go
+++ HELLO-WORLD/main.go
@@ -1,62 +1,79 @@
 package main
 
-import "log"
+import "net/http"
+import "fmt"
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
+//! Creating an INTERFACE
+// import "log"
 
-type Dog struct {
-	Name string
-	Breed string
-}
+// type Animal interface {
+// 	Says() string
+// 	NumberOfLegs() int
+// }
 
-type Gorilla struct {
-	Name string
-	Color string
-	NumberOfTeeth int
-}
+// type Dog struct {
+// 	Name string
+// 	Breed string
+// }
 
-func main() {
-	dog := Dog {
-		Name: "Samson",
-		Breed: "German Shepherd",
-	}
+// type Gorilla struct {
+// 	Name string
+// 	Color string
+// 	NumberOfTeeth int
+// }
 
-	PrintInfo(dog)
+// func main() {
+// 	dog := Dog {
+// 		Name: "Samson",
+// 		Breed: "German Shepherd",
+// 	}
 
-	gorilla := Gorilla {
-		Name: "King Kong",
-		Color: "Black",
-		NumberOfTeeth: 32,
-	}
+// 	PrintInfo(dog)
 
-	PrintInfo(gorilla)
-}
+// 	gorilla := Gorilla {
+// 		Name: "King Kong",
+// 		Color: "Black",
+// 		NumberOfTeeth: 32,
+// 	}
 
-func (d Dog) Says() string {
-	return "woof"
-}
+// 	PrintInfo(gorilla)
+// }
 
-func (d Dog) NumberOfLegs() int{
-	return 4
-}
-func (d Gorilla) Says() string {
-	return "grunt"
-}
+// func (d Dog) Says() string {
+// 	return "woof"
+// }
 
-func (d Gorilla) NumberOfLegs() int{
-	return 2
-}
+// func (d Dog) NumberOfLegs() int{
+// 	return 4
+// }
+// func (d Gorilla) Says() string {
+// 	return "grunt"
+// }
 
-func PrintInfo(a Animal){
-	log.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
-}
+// func (d Gorilla) NumberOfLegs() int{
+// 	return 2
+// }
+// //! I had to import "log" for log to work
+// func PrintInfo(a Animal){
+// 	log.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+// }
 
-
-// import "fmt"
-
+//! HELLO WORLD.....
+//? Take 1
 // func main(){
 // 	fmt.Println("Hello, World of GO!")
 // }
+
+//? Take 2
+func main(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		n, err := fmt.Fprintf(w, "Hello, World!")
+		// if there IS an error
+		if err != nil { 
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+}
